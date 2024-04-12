bbb07e7c92165c9115eee6e39131eccd8ea90534
WillChilds-Klein <willck93@gmail.com>
2024-04-12 20:35:20 +0000

    Add SDK+CRT build script, small example dir

---
+++ example/example.h
@@ -0,0 +1,7 @@
+#include <aws/core/Aws.h>
+
+namespace AwsDoc {
+    namespace STS {
+        bool getCallerIdentity(const Aws::Client::ClientConfiguration &clientConfig);
+    } // sts
+}  // AwsDoc
